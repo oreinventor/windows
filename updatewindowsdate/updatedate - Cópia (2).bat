9b07d7e80f8862778387d043f54a36a9f95fbd91
oreinventor <oreinventor@gmail.com>
2022-09-29 16:57:55 -0300

    initial2

--- updatewindowsdate/updatedate - Cópia (2).bat
+++
@@ -1,6 +0,0 @@
-@echo off
-date 21-08-2022
-net stop w32time
-net start w32time
-w32tm /config /reliable:YES /manualpeerlist:"ntps1.pads.ufrj.br time.google.com time.cloudflare.com time.facebook.com time.windows.com time.apple.com clepsydra.dec.com time-a-g.nist.gov" /syncfromflags:manual /update
-w32tm /resync /rediscover
--- updatewindowsdate/updatedate - Cópia.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-date 23-05-2022
-net stop w32time
-w32tm /config /reliable:YES /manualpeerlist:"ntps1.pads.ufrj.br time.google.com time.cloudflare.com time.facebook.com time.windows.com time.apple.com clepsydra.dec.com time-a-g.nist.gov ntp1.vniiftri.ru ntp.mobatime.ru" /syncfromflags:manual /update
-net start w32time
-w32tm /resync /rediscover
